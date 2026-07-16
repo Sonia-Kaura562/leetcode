ea00f78da38e2b78c5a83749a61e037b569f08a8
Sonia-Kaura562 <218599556+Sonia-Kaura562@users.noreply.github.com>
2026-07-16 22:55:13 +0530

    Time: 0 ms (100%), Space: 8.6 MB (80.38%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
-    int digit(int num) {
-        if(num <= 0 ) return 0;
-        return num%10 + digit(num/10);
-    }
     int addDigits(int num) {
-        if(num <= 9) return num;
-        return addDigits(digit(num));  
+        if(!num) return 0;
+        if(num%9 == 0) return 9;
+        return num%9;
     }
 };
