4c82d747892c4ae2df4dc979040d6298922defe4
Sonia-Kaura562 <218599556+Sonia-Kaura562@users.noreply.github.com>
2026-07-20 12:23:08 +0530

    Time: 3 ms (75.08%), Space: 19.4 MB (55.08%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,15 +1,15 @@
 class Solution {
 private: 
-    bool dfs(vector<vector<int>>& adj, vector<int>& vis, vector<int>& visPath, int node) {
+    bool dfs(vector<vector<int>>& adj, vector<int>& vis, int node) {
         for(int i = 0; i < adj[node].size(); i++) {
             if(!vis[adj[node][i]]) {
+                vis[adj[node][i]] = 2;
+                //visPath[adj[node][i]] = 1;
+                if(!dfs(adj, vis, adj[node][i])) return false;
                 vis[adj[node][i]] = 1;
-                visPath[adj[node][i]] = 1;
-                if(!dfs(adj, vis, visPath, adj[node][i])) return false;
-                visPath[adj[node][i]] = 0;
             }
             else {
-                if(visPath[adj[node][i]] == 1) {
+                if(vis[adj[node][i]] == 2) {
                         return false;
                 }
             }
@@ -22,7 +22,7 @@ public:
         if(numCourses == 0 or numCourses == 1) return true;
         if(prerequisites.size() == 0) return true;
         vector<int> vis(numCourses, 0);
-        vector<int> visPath(numCourses, 0);
+        //vector<int> visPath(numCourses, 0);
         vector<vector<int>> adj(numCourses);
 
         for(int i = 0; i < prerequisites.size(); i++) {
@@ -31,11 +31,9 @@ public:
 
         for(int i = 0; i < numCourses; i++) {
             if(vis[i] == 0) {
-                int node = i;
-                vis[node] = 1;
-                visPath[node] = 1;
-                if(!dfs(adj, vis, visPath, node)) return false;
-                visPath[node] = 0;
+                vis[i] = 2;
+                if(!dfs(adj, vis, i)) return false;
+                vis[i] = 1;
             }
         }
         
