c5ddffeea8832b32c60512f0edf8a47444284e25
Sonia-Kaura562 <218599556+Sonia-Kaura562@users.noreply.github.com>
2026-07-20 12:05:39 +0530

    Time: 4 ms (63.67%), Space: 19.5 MB (48.21%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,33 +1,43 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-        vector<int> indegree(numCourses, 0);
-        for(vector<int> num : prerequisites) {
-            int d = num[0];
-            int s = num[1];
-            adj[s].push_back(d);
-            indegree[d]++;
-        }
-
-        queue<int>q;
-        for(int i = 0; i < numCourses; i++) {
-            if(indegree[i] == 0) {
-               q.push(i);
+    bool dfs(vector<vector<int>>& adj, vector<int>& vis, vector<int>& visPath, int node) {
+        for(int i = 0; i < adj[node].size(); i++) {
+            if(!vis[adj[node][i]]) {
+                vis[adj[node][i]] = 1;
+                visPath[adj[node][i]] = 1;
+                if(!dfs(adj, vis, visPath, adj[node][i])) return false;
+                visPath[adj[node][i]] = 0;
             }
-        }
-        int c = 0;
-        while(!q.empty()) {
-            int course = q.front();
-            q.pop();
-            c++;
-            for(int num : adj[course]) {
-                indegree[num]--;
-                if(indegree[num] == 0) {
-                    q.push(num);
+            else {
+                if(visPath[adj[node][i]] == 1) {
+                        return false;
                 }
             }
         }
-        return c == numCourses;
+        return true;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        //int vis [numCourses] = {0};
+        if(numCourses == 0 or numCourses == 1) return true;
+        if(prerequisites.size() == 0) return true;
+        vector<int> vis(numCourses, 0);
+        vector<int> visPath(numCourses, 0);
+        vector<vector<int>> adj(numCourses);
+
+        for(int i = 0; i < prerequisites.size(); i++) {
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+
+        for(int i = 0; i < numCourses; i++) {
+            if(vis[i] == 0) {
+                int node = i;
+                vis[node] = 1;
+                visPath[node] = 1;
+                if(!dfs(adj, vis, visPath, node)) return false;
+                visPath[node] = 0;
+            }
+        }
+        
+        return true;
     }
 };
