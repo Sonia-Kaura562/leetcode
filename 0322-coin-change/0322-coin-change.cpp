52b00b7ba784568d673305ffa07ebb54fdcda681
Sonia-Kaura562 <218599556+Sonia-Kaura562@users.noreply.github.com>
2026-07-25 23:53:51 +0530

    Time: 51 ms (24.69%), Space: 49 MB (36.97%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,39 +1,25 @@
 class Solution {
 private:
-    int solve(vector<int>& coins, int amount, int index,
-              vector<vector<int>>& memo) {
-        int size = coins.size() - 1;
-        if (amount == 0)
-            return 0;
-
-        if (index == size) {
-            if (amount % coins[size] == 0)
-                return amount / coins[size];
+    int solve(vector<int>& coins, int amount, int index, vector<vector<int>>& memo) {
+        if(memo[index][amount] != -1) {
+            return memo[index][amount];
+        }
+        if(!amount) return 0;
+        if(index == 0) {
+            if(amount % coins[0] == 0) return amount/coins[0];
             return 1e7;
         }
-
-        if (memo[index][amount] != -1)
-            return memo[index][amount];
-
         int take = 1e7;
-
-        if (coins[index] <= amount) {
+        if(coins[index] <= amount) {
             take = 1 + solve(coins, amount - coins[index], index, memo);
         }
-
-        int notTake = solve(coins, amount, index + 1, memo);
-
+        int notTake = solve(coins, amount, index - 1, memo);
         return memo[index][amount] = min(take, notTake);
     }
-
 public:
     int coinChange(vector<int>& coins, int amount) {
-
-        vector<vector<int>> memo(coins.size(),
-        vector<int>(amount + 1, -1));
-
-        int ans = solve(coins, amount, 0, memo);
-
-        return (ans >= 1e7) ? -1 : ans;
+        vector<vector<int>> memo(coins.size(), vector<int>(amount + 1, -1));
+        int result = solve(coins, amount, coins.size() - 1, memo);
+        return (result >= 1e7) ? -1 : result;
     }
 };
