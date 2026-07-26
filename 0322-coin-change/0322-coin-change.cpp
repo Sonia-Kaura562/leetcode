b7071efb5234485792c6f1459edbcf931092a37a
Sonia-Kaura562 <218599556+Sonia-Kaura562@users.noreply.github.com>
2026-07-26 18:56:01 +0530

    Time: 51 ms (24.82%), Space: 53.1 MB (10.96%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,25 +1,32 @@
 class Solution {
-private:
-    int solve(vector<int>& coins, int amount, int index, vector<vector<int>>& memo) {
-        if(memo[index][amount] != -1) {
-            return memo[index][amount];
-        }
-        if(!amount) return 0;
-        if(index == 0) {
-            if(amount % coins[0] == 0) return amount/coins[0];
-            return 1e7;
-        }
-        int take = 1e7;
-        if(coins[index] <= amount) {
-            take = 1 + solve(coins, amount - coins[index], index, memo);
-        }
-        int notTake = solve(coins, amount, index - 1, memo);
-        return memo[index][amount] = min(take, notTake);
-    }
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> memo(coins.size(), vector<int>(amount + 1, -1));
-        int result = solve(coins, amount, coins.size() - 1, memo);
-        return (result >= 1e7) ? -1 : result;
+
+        int n = coins.size();
+        const int INF = 1e7;
+
+        vector<vector<int>> tabu(n + 1, vector<int>(amount + 1, INF));
+
+        // Base case
+        for (int i = 0; i <= n; i++)
+            tabu[i][0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+
+                // Not take
+                tabu[i][j] = tabu[i - 1][j];
+
+                // Take
+                if (coins[i - 1] <= j) {
+                    tabu[i][j] = min(
+                        tabu[i][j],
+                        1 + tabu[i][j - coins[i - 1]]
+                    );
+                }
+            }
+        }
+
+        return (tabu[n][amount] >= INF) ? -1 : tabu[n][amount];
     }
 };
