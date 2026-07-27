5b7d325fba3e1150ae74a196cab6781b0721ed72
Sonia-Kaura562 <218599556+Sonia-Kaura562@users.noreply.github.com>
2026-07-27 14:38:20 +0530

    Time: 45 ms (38.64%), Space: 52.9 MB (12.91%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-
-        int n = coins.size();
-        const int INF = 1e7;
-
-        vector<vector<int>> tabu(n + 1, vector<int>(amount + 1, INF));
-
-        // Base case
-        for (int i = 0; i <= n; i++)
+        int size = coins.size();
+        vector<vector<int>>tabu(size + 1, vector<int>(amount + 1, 1e7));
+        for(int i = 0; i <= size; i++) {
             tabu[i][0] = 0;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= amount; j++) {
-
-                // Not take
-                tabu[i][j] = tabu[i - 1][j];
-
-                // Take
-                if (coins[i - 1] <= j) {
-                    tabu[i][j] = min(
-                        tabu[i][j],
-                        1 + tabu[i][j - coins[i - 1]]
-                    );
-                }
+        }
+        for(int i  = 1; i <= size; i++) {
+            for(int j = 1; j <= amount; j++) {
+                tabu[i][j] = tabu[i -1][j];
+                if(j >= coins[i-1])
+                tabu[i][j] = min(tabu[i][j], 1 + tabu[i][j - coins[i - 1]]);
             }
         }
 
-        return (tabu[n][amount] >= INF) ? -1 : tabu[n][amount];
+        return tabu[size][amount] >= 1e7 ? -1 : tabu[size][amount];
     }
 };
