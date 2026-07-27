9f48256eac084e06907ed792ef876741a3f622e4
Sonia-Kaura562 <218599556+Sonia-Kaura562@users.noreply.github.com>
2026-07-27 16:14:42 +0530

    Time: 341 ms (5.08%), Space: 23.5 MB (98.23%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,15 +3,10 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
         for(int i = 0; i < intervals.size() - 1; i++) {
-            for(int j = i  + 1; j < intervals.size(); j++) {
-                if(intervals[i][1] >= intervals[j][0]) {
-                    intervals[i][1] = max(intervals[j][1], intervals[i][1]);
-                    intervals.erase(intervals.begin() + j);
-                    j--;
-                }
-                else {
-                    break;
-                }
+            if(intervals[i][1] >= intervals[i+1][0]) {
+                intervals[i][1] = max(intervals[i + 1][1], intervals[i][1]);
+                intervals.erase(intervals.begin() + i + 1);
+                i--;
             }
         }
 
