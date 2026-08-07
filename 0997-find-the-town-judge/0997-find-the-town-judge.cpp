d184e9c4b402d89383099b7ae7995837d16a60a4
Sonia-Kaura562 <218599556+Sonia-Kaura562@users.noreply.github.com>
2026-08-07 19:02:52 +0530

    Time: 25 ms (34.65%), Space: 73.3 MB (24.67%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -4,11 +4,9 @@ public:
         int size = trust.size();
         vector<int> adj(n + 1, 0);
         vector<int> indegree(n + 1, 0);
-        for(int i = 0; i < size; i++) {
-            int a = trust[i][0];
-            int b = trust[i][1];
-            adj[a]++;
-            indegree[b]++;
+        for(auto it : trust) {
+            adj[it[0]]++;
+            indegree[it[1]]++;
         }
 
         for(int i = 1; i < n + 1; i++) {
