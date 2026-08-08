9df25be3ac7586646a631570719242c4d7d8b7d2
Sonia-Kaura562 <218599556+Sonia-Kaura562@users.noreply.github.com>
2026-08-09 00:02:05 +0530

    Time: 1 ms (80.36%), Space: 18.3 MB (56.47%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,39 +1,40 @@
 class Solution {
 private:
-    bool dfs(int curr, vector<vector<int>>& adj, vector<int>& order, vector<int>& vis) {
-        vis[curr] = 2;
-        for(int i = 0; i < adj[curr].size(); i++) {
-            int b = adj[curr][i];
-            if(vis[b] == 2) {
-                return false;
-            }
-            else if(!vis[b]) {
-                if(!dfs(b, adj, order, vis)) return false;
+    void bfs(vector<vector<int>>& adj, vector<int>& indegree, vector<int>& order, queue<int>& q) {
+        while(!q.empty()) {
+            int fr = q.front();
+            order.push_back(fr);
+            q.pop();
+            for(int i = 0; i < adj[fr].size(); i++) {
+                int b = adj[fr][i];
+                indegree[b]--;
+                if(!indegree[b]) {
+                    q.push(b);
+                }
             }
         }
-        order.push_back(curr);
-        vis[curr] = 1;
-        return true;
     }
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>>adj(numCourses);
+        vector<int>indegree(numCourses, 0);
         vector<int>order;
-        vector<int>vis(numCourses, 0);
+        queue<int>q;
         int size = prerequisites.size();
         for(int i = 0; i < size; i++) {
             int a = prerequisites[i][0];
             int b = prerequisites[i][1];
             adj[b].push_back(a);
+            indegree[a]++;
         }
         
         for(int i = 0; i < numCourses; i++) {
-            if(!vis[i]) {
-                if(!dfs(i, adj, order, vis)) return {};
+            if(!indegree[i]) {
+                q.push(i);
             }
         }
-        reverse(order.begin(), order.end());
-        //return (order.size() == numCourses) ? order : vector<int>{};
-        return order;
+        bfs(adj, indegree, order, q);
+        return (order.size() == numCourses) ? order : vector<int>{};
+        
     }
 };
