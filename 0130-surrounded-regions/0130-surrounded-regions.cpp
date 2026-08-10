aa98af5f7506f55e3f5c7ea3c853aa22eaf45554
Sonia-Kaura562 <218599556+Sonia-Kaura562@users.noreply.github.com>
2026-08-10 20:12:47 +0530

    Time: 0 ms (100%), Space: 14.4 MB (69.01%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,81 +1,41 @@
 class Solution {
 private:
-    bool dfs(int row, int col, vector<vector<char>>& board,
-             vector<vector<int>>& vis) {
-
+    void dfs(int r, int c, vector<vector<char>>& board, vector<vector<int>>& vis) {
         int m = board.size();
         int n = board[0].size();
-
-        vis[row][col] = 1;
-
-        bool touchBoundary = (row == 0 || col == 0 ||
-                              row == m - 1 || col == n - 1);
-
+        vis[r][c] = 1;
         int adj[4][2] = {
             {0, -1},
             {-1, 0},
             {0, 1},
             {1, 0}
         };
+        for(int i = 0; i < 4; i++) {
+            int row = r + adj[i][0];
+            int col = c + adj[i][1];
 
-        for (int i = 0; i < 4; i++) {
-            int r = row + adj[i][0];
-            int c = col + adj[i][1];
-
-            if (r >= 0 && c >= 0 && r < m && c < n &&
-                board[r][c] == 'O' && !vis[r][c]) {
-
-                if (dfs(r, c, board, vis))
-                    touchBoundary = true;
+            if(row >= 0 and col >= 0 and row < m and col < n and !vis[row][col] and board[row][col] == 'O') {
+                dfs(row, col, board, vis);
             }
         }
-
-        return touchBoundary;
     }
-
 public:
     void solve(vector<vector<char>>& board) {
         int m = board.size();
         int n = board[0].size();
-
-        vector<vector<int>> vis(m, vector<int>(n, 0));
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-
-                if (board[i][j] == 'O' && !vis[i][j]) {
-
-                    
-
+        vector<vector<int>>vis(m, vector<int>(n, 0));
+        for(int i = 0; i < m; i++) {
+            for(int j  = 0; j < n; j++) {
+                if((!vis[i][j] and board[i][j] == 'O') and (i == 0 or j == 0 or i == m - 1 or j == n - 1)) {
                     dfs(i, j, board, vis);
+                } 
+            }
+        }
 
-                    bool boundary = false;
-
-                    for (int r = 0; r < m; r++) {
-                        for (int c = 0; c < n; c++) {
-                            if (vis[r][c] == 1 &&
-                                (r == 0 || c == 0 ||
-                                 r == m - 1 || c == n - 1)) {
-                                boundary = true;
-                            }
-                        }
-                    }
-
-                    if (!boundary) {
-                        for (int r = 0; r < m; r++) {
-                            for (int c = 0; c < n; c++) {
-                                if (vis[r][c] == 1)
-                                    board[r][c] = 'X';
-                            }
-                        }
-                    }
-
-                    for (int r = 0; r < m; r++) {
-                        for (int c = 0; c < n; c++) {
-                            if (vis[r][c] == 1)
-                                vis[r][c] = 2;
-                        }
-                    }
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(!vis[i][j] and board[i][j] == 'O') {
+                    board[i][j] = 'X';
                 }
             }
         }
