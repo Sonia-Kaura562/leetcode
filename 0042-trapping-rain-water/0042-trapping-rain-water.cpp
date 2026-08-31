9c5c2d7046760e4582a721b271d3a854fca775a7
Sonia-Kaura562 <218599556+Sonia-Kaura562@users.noreply.github.com>
2026-08-31 21:36:44 +0530

    Time: 2 ms (18.59%), Space: 27.2 MB (25.98%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -4,19 +4,17 @@ public:
         int size = height.size();
         vector<int> leftMax(size, 0);
         vector<int> rightMax(size, 0);
+        leftMax[0] = height[0]; 
+        rightMax[size - 1] = height[size - 1];
         for(int i = 1; i < size; i++) {
-            leftMax[i] = max(height[i - 1], leftMax[i - 1]);
+            leftMax[i] = max(height[i], leftMax[i - 1]);
         }
         for(int i = size - 2; i >= 0; i--) {
-            rightMax[i] = max(height[i + 1], rightMax[i + 1]);
+            rightMax[i] = max(height[i], rightMax[i + 1]);
         }
         int sum = 0;
         for(int i = 1; i < size - 1; i++) {
-            //if(height[i] < leftMax[i] or height[i] < rightMax[i]) {
-                // int left = leftMax[i];
-                // int right = rightMax[i];
-                sum += max(0, min(leftMax[i], rightMax[i]) - height[i]);
-            //}
+                sum +=  (min(leftMax[i], rightMax[i]) - height[i]);
         }
         return sum;
     }
