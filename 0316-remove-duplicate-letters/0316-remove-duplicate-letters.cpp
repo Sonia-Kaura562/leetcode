be123ee42396f9007648d930f89f29fd84001c07
Sonia-Kaura562 <218599556+Sonia-Kaura562@users.noreply.github.com>
2026-09-12 11:40:44 +0530

    Time: 3 ms (15.66%), Space: 9.6 MB (35.26%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -1,28 +1,39 @@
 class Solution {
-public:
-    string removeDuplicateLetters(string s) {
-        vector<bool> vis(26, false);
-        vector<int>count(26, 0);
+    public:
+     string removeDuplicateLetters(string s) {
+        int size = s.size();
+        vector<int>count(27, 0);
+        vector<bool>vis(27, 0);
         stack<char>st;
-        string result;
-        for(char ch : s) {
-            count[ch - 'a']++;
+        for(int i = 0; i < size; i++) {
+            int k = s[i] - 'a';
+            count[k]++;
         }
-        for(char ch : s) {
-            count[ch - 'a']--;
-            if(vis[ch - 'a']) continue;
-            while(!st.empty() && st.top() > ch && count[st.top() - 'a'] > 0) {
-                vis[st.top() - 'a'] = false;
+
+        for(auto i : s) {
+            if(vis[i - 'a'] == 1) {
+                count[i - 'a']--;
+                continue;
+            }
+            while(!st.empty() and st.top() > i) {
+                if(count[st.top() - 'a'] <= 0) break;
+                vis[st.top() - 'a'] = 0;
                 st.pop();
             }
-            st.push(ch);
-            vis[ch - 'a'] = true;
+            st.push(i);
+            vis[i - 'a'] = 1;
+            count[i - 'a']--;
         }
+        string ans;
+        vector<char>res;
         while(!st.empty()) {
-            result += st.top();
+            res.push_back(st.top());
             st.pop();
         }
-        reverse(result.begin(), result.end());
-        return result;
+        reverse(res.begin(), res.end());
+        for(auto i : res) {
+            ans += i;
+        }
+        return ans;
     }
 };
