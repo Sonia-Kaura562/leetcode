1003ae3d9af71e6e6bced329d03feaf37eb8da3a
Sonia-Kaura562 <218599556+Sonia-Kaura562@users.noreply.github.com>
2026-09-14 14:33:15 +0530

    Time: 0 ms (100%), Space: 87.1 MB (96.06%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        if(bills[0] != 5) return false;
+        int n = bills.size();
+        int total[3] = {0, 0, 0};
+        for(int i = 0; i < n; i++) {
+            if(bills[i] == 5) {
+                total[0]++;
+            }
+            else {
+                if(bills[i] == 10) {
+                    if(total[0] > 0) {
+                        total[0]--;
+                        total[1]++;
+                    }
+                    else return false;
+                }
+                else if(bills[i] == 20) {
+                    if(total[1]  && total[0]) {
+                        total[0]--;
+                        total[1]--;
+                        total[2]++;
+                    }   
+                    else if(total[0] >= 3) {
+                        total[0] -= 3;
+                        total[2]++;
+                    }
+                    else return false;
+                }
+            }
+        }
+        return true;
+    }
+};
